d27320f5fe99524908113865c236dd832ad882e7
SHUBHAM6287 <112172257+SHUBHAM6287@users.noreply.github.com>
2023-10-15 15:18:23 +0530

    code

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+// database account details are hidden due to security purposes.
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+?>
